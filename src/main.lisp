f4f3cd0240d47de61f936629d3873748188ee372
Solomon Bloch <solomonbloch@gmail.com>
2021-03-15 21:30:19 -0400

    Implemented initializing and api-request.

--- src/main.lisp
+++ src/main.lisp
@@ -1,40 +1,55 @@
 (defpackage hue
-  (:import-from :jonathan :parse)
+  (:import-from :jonathan :parse :to-json)
   (:use :cl))
 (in-package :hue)
 
 (defclass bridge ()
-  ((address
-    :accessor address
-    :initarg :address)
+  ((ip
+    :accessor ip
+    :initarg :ip)
    (username
     :accessor username
     :initarg :username)
    (token-path
     :accessor token-path
-    :initform (merge-pathnames ".cache/bridge-auth" (user-homedir-pathname))
-    :initarg :token-path)))
+    :initform (merge-pathnames ".cache/hue-auth" (user-homedir-pathname))
+    :initarg :token-path)
+   (device-type
+    :accessor device-type
+    :initarg :device-type)))
 
 (defun bridge-list ()
   (parse (dex:get "https://discovery.meethue.com") :as :alist))
 
 
-(defun get-key-bridge (ip)
-  "Press bridge button, then this connects to bridge and return bridge object."
+(defmethod write-token ((bridge bridge))
+  (let ((json (to-json `(:ip ,(ip bridge) :username ,(username bridge) :device-type ,(device-type bridge)))))
+    (with-open-file (stream (token-path bridge)
+                            :direction :output
+                            :if-exists :supersede)
+      (write-line json stream))))
+
+(defun initialize-bridge (ip &key (device-type "hue"))
+  "Press bridge button, then this connects to bridge, prints hue-auth file, and returns bridge."
   (let ((response
-          (car (parse (dex:post (concatenate 'string "http://" ip "/api") :content
-                                (jonathan:to-json '(:devicetype  "hue-lisp")))
+          (car (parse (dex:post (concatenate 'string "http://" ip "/api")
+                                :content (format nil "{\"devicetype\":\"~a\"}" device-type))
                       :as :hash-table))))
     (if (gethash "error" response)
         "BUTTON NOT CLICKED."
-        (make-instance 'bridge
-                       :address ip
-                       :username (gethash "username" (gethash "success" response))))))
+        (let (((make-instance 'bridge
+                              :ip ip
+                              :username (gethash "username" (gethash "success" response))
+                              :device-type device-type)))))))
 
+(defun load-bridge (&key (token-path (merge-pathnames ".cache/hue-auth" (user-homedir-pathname))))
+  (let ((token-hash (parse (uiop:read-file-string token-path)
+                           :as :hash-table)))
+    (make-instance 'bridge
+                   :ip (gethash "IP" token-hash)
+                   :username (gethash "USERNAME" token-hash)
+                   :device-type (gethash "DEVICE-TYPE" token-hash))))
 
-(defun slurp (path)
-  (with-open-file (st path :direction :input
-                           :if-does-not-exist nil)
-    (when (streamp st)
-      (loop for line = (read-line st nil)
-            while line collect line))))
+(defmethod api-request ((bridge bridge) api &key (method 'get) content)
+  (parse (dex:request (format nil "http://~a/api/~a/~a" (ip bridge) (username bridge) api)
+                      :method method :content content) :as :alist))
